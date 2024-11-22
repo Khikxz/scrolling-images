dcf2982a83b4c50eea457e86d780bcb82ca8275b
Khikxz <khikxz.corbix13542@gmail.com>
2024-11-22 12:41:12 +0800

    initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const card = document.querySelectorAll(".card");
+window.addEventListener("scroll", animateCards);
+
+function animateCards(){
+    const checkingBottom = (window.innerHeight/4) * (125/25);
+    card.forEach(singleCard =>{
+        const cardTop = singleCard.getBoundingClientRect().top;
+        let toSlidIn = cardTop < checkingBottom;
+
+        if(toSlidIn){
+            singleCard.classList.add("slidingIn");
+        }else{
+            singleCard.classList.remove("slidingIn");
+        }
+    })
+}
